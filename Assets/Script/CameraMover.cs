3ca649b584568c9781fa6d373a6d88cffde1208a
zhouy546 <zhouyunqi546@gmail.com>
2018-10-14 11:15:00 +0800

    Initial commit

---
+++ Assets/Script/CameraMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+    public Transform CameraDefaultTrans;
+    public List<Node> nodes = new List<Node>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            MoveCameraToPos(nodes[0].CameraTrans);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            MoveCameraToPos(nodes[1].CameraTrans);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            MoveCameraToPos(nodes[2].CameraTrans);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape)) {
+            MoveCameraToPos(CameraDefaultTrans);
+        }
+	}
+
+    public void MoveCameraToPos(Transform transform) {
+        this.transform.SetParent(transform);
+
+        LeanTween.rotateLocal(this.gameObject, Vector3.zero, 1f);
+        LeanTween.moveLocal(this.gameObject, Vector3.zero, 1f);
+    }
+}
